05fea096a46e3db72bb42991b852a77729d0393a
Roman <roman@mgstr.ru>
2023-10-24 22:55:42 +0600

    feat: add api services

---
+++ react/src/service/apiService.ts
@@ -0,0 +1,27 @@
+import { RequestParams } from '../types';
+
+const API_KEY = 'b0706de8-b3da-4a9b-ac07-af4a3fec399a';
+const NEWS_ENDPOINT = 'https://content.guardianapis.com/search';
+const ARTICLE_ENDPOINT = 'https://content.guardianapis.com/';
+
+export class ApiService {
+  public async getNews(params: RequestParams) {
+    const url = `${NEWS_ENDPOINT}?q=${params.keyword}&order-by=${params.sort}&page-size=${params.limit}&page=${params.page}&api-key=${API_KEY}&show-fields=all`;
+    const response = await fetch(url);
+    if (response.status === 200) {
+      const resFromJSON = await response.json();
+      return resFromJSON.response.results;
+    }
+    throw new Error('There is something wrong with the server!');
+  }
+
+  public async getCurrentArticle(id: string) {
+    const url = `${ARTICLE_ENDPOINT}${id}?api-key=${API_KEY}&show-fields=all`;
+    const response = await fetch(url);
+    if (response.status === 200) {
+      const resFromJSON = await response.json();
+      return resFromJSON.response.content;
+    }
+    throw new Error('There is something wrong with the server!');
+  }
+}
