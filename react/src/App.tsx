52eea9106e59739d11514df0d87f1f8f054e8bc8
Roman <roman@mgstr.ru>
2023-10-28 14:37:10 +0600

    refactor: move App to function component

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,63 +1,47 @@
-import { Component } from 'react';
 import { Search } from './components/search';
 import { NewsSection } from './components/news-section';
-import { AppProps, AppState, ArticleInCatalog, StorageValues } from './types';
+import { ArticleInCatalog, StorageValues } from './types';
 import { ApiService } from './service/apiService';
 import { Spinner } from './components/spinner';
 import { Navigation } from './components/navigation';
+import { useEffect, useState } from 'react';
 
-export default class App extends Component<AppProps, AppState> {
-  state: AppState = {
-    keyword: localStorage.getItem(StorageValues.Keyword) || '',
-    isLoading: true,
-    errorMsg: null,
-    news: [],
-  };
+export default function App() {
+  const [keyword, setKeyword] = useState<string>(
+    localStorage.getItem(StorageValues.Keyword) || ''
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [errorMsg, setErrorMsg] = useState<null | string>(null);
+  const [news, setNews] = useState<ArticleInCatalog[]>([]);
 
-  componentDidMount = () => {
-    this.fetchNews(this.state.keyword);
-  };
+  useEffect(() => {
+    async function fetchNews() {
+      localStorage.setItem(StorageValues.Keyword, keyword);
+      setIsLoading(true);
+      const apiService = new ApiService();
+      const newsArr: ArticleInCatalog[] = await apiService.getNews(keyword);
+      setNews(newsArr);
+      setIsLoading(false);
+    }
+    fetchNews();
+  }, [keyword]);
 
-  componentDidUpdate = () => {
-    if (this.state.errorMsg) throw new Error('Manually envoked error');
-  };
+  useEffect(() => {
+    if (errorMsg) throw new Error(errorMsg);
+  });
 
-  fetchNews = async (word: string) => {
-    this.setState({ isLoading: true, keyword: word });
-    const apiService = new ApiService();
-    const newsArr: ArticleInCatalog[] = await apiService.getNews(word);
-    this.setState({ isLoading: false, news: newsArr });
-  };
-
-  setKeyword = (word: string) => {
-    this.setState({
-      keyword: word,
-    });
-    localStorage.setItem(StorageValues.Keyword, word);
-    this.fetchNews(word);
-  };
-
-  render() {
-    return (
-      <section className="flex flex-col items-stretch">
-        <Navigation />
-        <Search
-          word={this.state.keyword}
-          keywordCallback={(word) => {
-            this.setKeyword(word);
-          }}
-        />
-        {this.state.isLoading && <Spinner />}
-        {!this.state.isLoading && <NewsSection newsBatch={this.state.news} />}
-        <button
-          className="m-2 p-2 text-white	bg-red-600 rounded-2xl"
-          onClick={() => {
-            this.setState({ errorMsg: 'Manually envoked error' });
-          }}
-        >
-          Throw error
-        </button>
-      </section>
-    );
-  }
+  return (
+    <section className="flex flex-col items-stretch">
+      <Navigation />
+      <Search word={keyword} keywordCallback={(word) => setKeyword(word)} />
+      {isLoading && <Spinner />}
+      {!isLoading && <NewsSection newsBatch={news} />}
+      <button
+        className="m-2 p-2 text-white	bg-red-600 rounded-2xl"
+        onClick={() => setErrorMsg('Manually envoked error')}
+      >
+        Throw error
+      </button>
+    </section>
+  );
 }
