1b56f37791c7a32d2fdb0775d235a4472dc6664b
Roman Shevyakov <rs.8@bk.ru>
2023-11-02 12:20:37 +0600

    feat: add new fields to form

--- react/src/components/search.tsx
+++ react/src/components/search.tsx
@@ -3,8 +3,8 @@ import { Form } from 'react-router-dom';
 import { StorageValues } from '../types';
 
 // interface SearchProps {
-// word: string;
-// keywordCallback: (word: string) => void;
+//   params: object;
+//   paramsCallback: (params: object) => void;
 // }
 
 export function Search() {
@@ -12,15 +12,37 @@ export function Search() {
 
   return (
     <section className="rounded-2xl bg-slate-500 text-white m-2 p-2">
-      <Form className="flex justify-center gap-2">
+      <Form
+        className="flex justify-center gap-2"
+        onSubmit={() => {
+          // console.log(e.target.sort.value);
+        }}
+      >
         <input
-          ref={searchInput}
           placeholder="crocodile?"
           className="text-black px-1 rounded"
           type="text"
           name="q"
           defaultValue={localStorage.getItem(StorageValues.Keyword) || ''}
         />
+        <select
+          ref={searchInput}
+          className="text-black px-1 rounded"
+          name="limit"
+          defaultValue="10"
+        >
+          <option value="10">10 items per page</option>
+          <option value="20">20 items per page</option>
+        </select>
+        <select
+          className="text-black px-1 rounded"
+          name="sort"
+          defaultValue="newest"
+        >
+          <option value="newest">Show newest first</option>
+          <option value="oldest">Show oldest first</option>
+          <option value="relevance">Sort by relevance</option>
+        </select>
         <input type="submit" value="Search" />
       </Form>
     </section>
