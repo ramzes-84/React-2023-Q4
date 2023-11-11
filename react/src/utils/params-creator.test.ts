008619048bf379bc98f94325ee9ea25dea421cea
Roman Shevyakov <roman@mgstr.ru>
2023-11-11 18:45:02 +0600

    test: add tests for params creator

--- react/src/utils/params-creator.test.ts
+++ react/src/utils/params-creator.test.ts
@@ -1,52 +1,41 @@
-import { expect, test, describe } from 'vitest';
-// import { paramsCreator } from './params-creator';
-// import { AppUrlParams, RequestParams } from '../types';
+import { expect, it, describe, vi } from 'vitest';
+import { paramsCreator } from './params-creator';
+import { PageLimitValue, RequestParams, Sort } from '../types';
 
 describe.concurrent('Config creator function tests', () => {
-  test('Should return default values', () => {
-    // const badParam2 = {
-    //   _search: 'search',
-    //   get search() {
-    //     return this._search;
-    //   },
-    //   get [AppUrlParams.Limit]() {
-    //     return null;
-    //   },
-    //   get [AppUrlParams.Page]() {
-    //     return null;
-    //   },
-    //   get [AppUrlParams.Sort]() {
-    //     return null;
-    //   },
-    // };
-    // const badParam1 = {};
-
-    // console.log(badParam2.get('search'));
-
-    // const resultParams = paramsCreator(
-    //   badParam1 as RequestParams,
-    //   badParam2 as URLSearchParams
-    // );
-    // expect(resultParams.q).toEqual('');
-    expect(true).toBeTruthy();
+  it('Should return default values when LS & url search params not provided', () => {
+    const badParam2 = {
+      get: vi.fn(),
+    };
+    const badParam1 = {};
+    const resultParams = paramsCreator(
+      badParam1 as RequestParams,
+      badParam2 as unknown as URLSearchParams
+    );
+    expect(resultParams.q).toEqual('');
+    expect(resultParams.limit).toEqual(PageLimitValue.ten);
+    expect(resultParams.page).toEqual('1');
+    expect(resultParams.sort).toEqual(Sort.Newest);
   });
-  // test('Should return null as first pagination section', () => {
-  //   const { firstPage } = paginationMapper(1, 100);
-  //   expect(firstPage).toBeNull();
-  // });
-  // test('Should return last pagination section', () => {
-  //   const { lastPage } = paginationMapper(10, 100);
-  //   expect(lastPage).toEqual(100);
-  // });
-  // test('Should return null as last pagination section', () => {
-  //   const { lastPage } = paginationMapper(100, 100);
-  //   expect(lastPage).toBeNull();
-  // });
-  // test('Should return middle pagination section correctly', () => {
-  //   const currPage = 17;
-  //   const totalPages = 30;
-  //   const { middleSegment } = paginationMapper(currPage, totalPages);
-  //   expect(middleSegment[0]).toEqual(currPage - 4);
-  //   expect(middleSegment[8]).toEqual(currPage + 4);
-  // });
+
+  it('Should return savedParams if provided', () => {
+    const savedParams: RequestParams = {
+      limit: PageLimitValue.fifty,
+      sort: Sort.Oldest,
+      page: '50',
+      q: 'query',
+    };
+    const badUrlParams = {
+      get: vi.fn(),
+    };
+
+    const resultParams = paramsCreator(
+      savedParams as RequestParams,
+      badUrlParams as unknown as URLSearchParams
+    );
+    expect(resultParams.q).toEqual('query');
+    expect(resultParams.limit).toEqual(PageLimitValue.fifty);
+    expect(resultParams.page).toEqual('50');
+    expect(resultParams.sort).toEqual(Sort.Oldest);
+  });
 });
