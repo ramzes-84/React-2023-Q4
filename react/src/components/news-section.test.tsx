34ca5236a1a21541e7468efc85690bc3551e80cb
Roman Shevyakov <roman@mgstr.ru>
2023-11-12 13:59:29 +0600

    test: add test

--- react/src/components/news-section.test.tsx
+++ react/src/components/news-section.test.tsx
@@ -20,8 +20,50 @@ vi.mock('./pagination', () => {
   };
 });
 vi.mock('../hooks/context-check', () => {
-  const articleResponse = {
-    id: 'id',
+  const articleResponse1 = {
+    id: 'id1',
+    type: 'type',
+    sectionId: 'sectionID',
+    sectionName: 'Australia news',
+    webPublicationDate: '2023-11-10T03:39:59Z',
+    webTitle: 'webTitle',
+    webUrl: 'https://www.test.com/',
+    apiUrl: 'https://www.test.com/',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+    fields: {
+      headline: 'headline',
+      standfirst: 'standfirst',
+      trailText: 'Follow live',
+      byline: 'Cait Kelly (now) and  Rafqa Touma (earlier)',
+      main: 'main',
+      wordcount: 'wordcount',
+      firstPublicationDate: 'firstPublicationDate',
+      isInappropriateForSponsorship: 'false',
+      isPremoderated: 'false',
+      lastModified: '2023-11-10T03:42:58Z',
+      productionOffice: 'AUS',
+      publication: 'theguardian.com',
+      shortUrl: 'https://www.theguardian.com/p/pae44',
+      shouldHideAdverts: 'false',
+      showInRelatedContent: 'true',
+      thumbnail: 'https://test.com/',
+      legallySensitive: 'false',
+      lang: 'en',
+      isLive: 'true',
+      bodyText: '',
+      charCount: '44957',
+      shouldHideReaderRevenue: 'false',
+      showAffiliateLinks: 'false',
+      bylineHtml: 'bylineHtml',
+      body: 'body',
+      newspaperPageNumber: '',
+      newspaperEditionDate: '',
+    },
+  };
+  const articleResponse2 = {
+    id: 'id2',
     type: 'type',
     sectionId: 'sectionID',
     sectionName: 'Australia news',
@@ -63,22 +105,35 @@ vi.mock('../hooks/context-check', () => {
     },
   };
   return {
-    useContextChecker: vi.fn().mockReturnValue({ news: [articleResponse] }),
+    useContextChecker: vi
+      .fn()
+      .mockReturnValueOnce({ news: [articleResponse1, articleResponse2] })
+      .mockReturnValueOnce({ news: [] }),
   };
 });
 
-describe('NewsSection component should render main page elements: card & pagination if there are articles', () => {
-  it('', () => {
+describe('NewsSection component should render main page elements: card & pagination if there are articles', async () => {
+  it('Verify that the component renders the specified number of cards', () => {
     act(() => {
       render(<NewsSection />);
     });
 
-    const cards = screen.getByText('Article Card');
+    const cards = screen.getAllByText('Article Card');
     const pagination = screen.getByText('Pagination buttons');
 
-    expect(cards).toBeInTheDocument();
-    expect(cards).toBeVisible();
+    expect(cards.length).toEqual(2);
     expect(pagination).toBeInTheDocument();
-    expect(pagination).toBeVisible();
+  });
+});
+
+describe('NewsSection component should render warning when there are no data', async () => {
+  it('Check that an appropriate message is displayed if no cards are present', () => {
+    act(() => {
+      render(<NewsSection />);
+    });
+
+    const nothingMsg = screen.getByText('Nothing was found');
+
+    expect(nothingMsg).toBeInTheDocument();
   });
 });
