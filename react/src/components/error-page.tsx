835d90958fb4f0583f805cbd4d3dba1d4cd31124
Roman <roman@mgstr.ru>
2023-11-16 10:29:33 +0600

    feat: add styles to error page

--- react/src/components/error-page.tsx
+++ react/src/components/error-page.tsx
@@ -7,7 +7,7 @@ export function ErrorPage() {
   return (
     <>
       <Navigation />
-      <div className="flex flex-col max-w-xs mx-auto">
+      <div className="flex flex-col max-w-xs mx-auto my-4 bg-yellow-200 p-6 rounded-xl">
         <p className="text-lg text-center">
           There is an error in application:
           <i>{(error as Error).message}</i>
