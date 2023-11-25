61444264e803ead53cb6deded019b8ba8699d2f0
Roman <roman@mgstr.ru>
2023-11-25 21:51:54 +0600

    refactor: delete next folder

--- news/components/spinner.tsx
+++
@@ -1,7 +0,0 @@
-export function Spinner() {
-  return (
-    <div className="self-center">
-      <img src="/spinner.gif" alt="Loading..." width={200} />
-    </div>
-  );
-}
--- news/pages/article/[...id]/index.tsx
+++
@@ -1,5 +0,0 @@
-import { SingleView } from "@/components/single-view";
-
-export default function Page() {
-  return <SingleView />;
-}
