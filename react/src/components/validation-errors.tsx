0d9462bee7ba55d9b4540fd03259418faeb6398f
Roman <roman@mgstr.ru>
2023-12-01 08:52:58 +0600

    feat: improve validation

--- react/src/components/validation-errors.tsx
+++ react/src/components/validation-errors.tsx
@@ -1,12 +1,15 @@
 type ValidationErrorsProps = {
   errArr: string[];
 };
+
 export const ValidationErrors = ({ errArr }: ValidationErrorsProps) => {
   const errList = errArr.map((err) => <li key={err}>{err}</li>);
   return (
-    <ul className="list-disc px-3">
-      Please correct these fields:
-      {errList}
-    </ul>
+    <div className="relative w-80">
+      <ul className="absolute text-white border px-5 py-1 rounded-lg list-disc">
+        Please correct these fields:
+        {errList}
+      </ul>
+    </div>
   );
 };
--- react/src/utils/usual-form-validator.ts
+++ react/src/utils/usual-form-validator.ts
@@ -4,12 +4,41 @@ import { Gender } from './types';
 export const usualFormSchema = object({
   age: number().required().positive().integer(),
   country: string().required(),
-  email: string().email().required(),
+  email: string().email('Email is not valid').required(),
   // file: string().required(),
   gender: string<Gender>().required(),
-  name: string().required(),
-  password: string().required(),
-  confirm: string().required('Confirm password is a required field'),
+  name: string()
+    .required()
+    .test({
+      name: '1stUpperCase',
+      skipAbsent: true,
+      test(value, ctx) {
+        if (value[0] === value[0].toUpperCase()) {
+          return true;
+        }
+        return ctx.createError({
+          message: 'Name should have first uppercased letter',
+        });
+      },
+    }),
+  password: string()
+    .min(4, 'Password must be at least 4 characters long')
+    .required(),
+  confirm: string()
+    .required('Confirm password is a required field')
+    .test({
+      name: 'checkTwoPasswds',
+      skipAbsent: true,
+      test(value, ctx) {
+        if (value === this.parent.password) {
+          return true;
+        }
+        return ctx.createError({
+          message: 'Passwords should be identical',
+        });
+      },
+    }),
+
   terms: boolean<true>().required(),
 });
 // : ObjectSchema<
