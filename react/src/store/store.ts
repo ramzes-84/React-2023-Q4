fbadb2db891731b7aa564139312b019867ad288d
Roman <roman@mgstr.ru>
2023-12-03 16:23:39 +0600

    feat: add slice for second form

--- react/src/store/store.ts
+++ react/src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { usualFormDataSlice } from './form-data-slice';
 import { countriesSlice } from './countries-slice';
+import { reactHookFormDataSlice } from './useform-data-slice';
 
 export const store = configureStore({
   reducer: {
     usualFormData: usualFormDataSlice.reducer,
+    reactHookFormData: reactHookFormDataSlice.reducer,
     countries: countriesSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
