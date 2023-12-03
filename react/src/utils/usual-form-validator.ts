850f38fddeba3735f92ceed7ddb5c6535efb946a
Roman <roman@mgstr.ru>
2023-12-03 11:00:00 +0600

    feat: some validation improvements

--- react/src/utils/usual-form-validator.ts
+++ react/src/utils/usual-form-validator.ts
@@ -2,61 +2,73 @@ import { boolean, number, object, string } from 'yup';
 import { Gender } from './types';
 // import { convertBase64 } from './base64-encode';
 
-export const usualFormSchema = object({
-  age: number()
-    .required()
-    .positive('Age field must be positive')
-    .integer('Age field must be a number'),
-  country: string().required(),
-  email: string().email('Email is not valid').required(),
-  file: string()
-    // .transform(async (value: FileList) => {
-    //   const file = value[0];
-    //   const base64 = await convertBase64(file);
-    //   console.log(base64);
-    //   return base64;
-    // })
-    .required('You did not add file'),
-  gender: string<Gender>().required(),
-  name: string()
-    .trim()
-    .required()
-    .test({
-      name: '1stUpperCase',
-      skipAbsent: true,
-      test(value, ctx) {
-        if (value[0] === value[0].toUpperCase()) {
-          return true;
-        }
-        return ctx.createError({
-          message: 'Name should have first uppercased letter',
-        });
-      },
-    }),
-  password: string()
-    .min(4, 'Password must be at least 4 characters long')
-    .matches(
-      /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{4,}/g,
-      {
-        message:
-          'Please ensure that password contains at least 1 number (0-9), 1 special character(!@#$%^&*), 1 uppercased (A-Z) and 1 lowercased (a-z) latin letter',
+export const age = number()
+  .required()
+  .positive('Age field must be positive')
+  .integer('Age field must be a number');
+export const country = string().required();
+export const email = string().email('Email is not valid').required();
+export const file = string()
+  // .transform(async (value: FileList) => {
+  //   const file = value[0];
+  //   const base64 = await convertBase64(file);
+  //   console.log(base64);
+  //   return base64;
+  // })
+  .required('You did not add file');
+export const gender = string<Gender>().required();
+export const name = string()
+  .trim()
+  .required()
+  .test({
+    name: '1stUpperCase',
+    skipAbsent: true,
+    test(value, ctx) {
+      if (value && value[0] === value[0].toUpperCase()) {
+        return true;
       }
-    )
-    .required('Password is a required field'),
-  confirm: string()
-    .required('Confirm password is a required field')
-    .test({
-      name: 'checkTwoPasswds',
-      skipAbsent: true,
-      test(value, ctx) {
-        if (value === this.parent.password) {
-          return true;
-        }
-        return ctx.createError({
-          message: 'Passwords should be identical',
-        });
-      },
-    }),
+      return ctx.createError({
+        message: 'Name should have first uppercased letter',
+      });
+    },
+  });
+export const password = string()
+  .min(4, 'Password must be at least 4 characters long')
+  .matches(
+    /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{4,}/g,
+    {
+      message:
+        'Please ensure that password contains at least 1 number (0-9), 1 special character(!@#$%^&*), 1 uppercased (A-Z) and 1 lowercased (a-z) latin letter',
+    }
+  )
+  .required('Password is a required field');
+export const confirm = string()
+  .required('Confirm password is a required field')
+  .test({
+    name: 'checkTwoPasswds',
+    skipAbsent: true,
+    test(value, ctx) {
+      if (value === this.parent.password) {
+        return true;
+      }
+      return ctx.createError({
+        message: 'Passwords should be identical',
+      });
+    },
+  });
 
-  terms: boolean<true>().required().isTrue('You have to agree with T&C'),
+export const terms = boolean<true>()
+  .required()
+  .isTrue('You have to agree with T&C');
+
+export const usualFormSchema = object({
+  age,
+  country,
+  email,
+  file,
+  gender,
+  name,
+  password,
+  confirm,
+  terms,
 });
